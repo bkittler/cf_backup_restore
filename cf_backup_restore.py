14d95a3a0d79c59c607bee6348bffd86b56a6d85
Benjamin Kittler <kittler@gmail.com>
2021-04-01 19:05:16 +0200

    version 0.1 - correct monitor import for empty header

--- cf_backup_restore.py
+++ cf_backup_restore.py
@@ -383,7 +383,6 @@ def importdata(cf, zone_dest):
                         else: datajson['follow_redirects'] = bool("True")
                         if datajson['allow_insecure'] == "False": datajson['allow_insecure'] = bool("")
                         else: datajson['allow_insecure'] = bool("True")
-                        #import pdb; pdb.set_trace()
                         if datajson['header'] == "None":
                                 datajson.pop('header', None)
                         exist = 0
